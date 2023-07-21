fdebea5443c52f11268c7736c9fe0fc6204fbc62
abdallahnegm <73608714+abdallahnegm@users.noreply.github.com>
2023-07-21 05:52:46 +0300

    Initial commit

---
+++ JS/index.js
@@ -0,0 +1,20 @@
+/**
+ *	www.templatemo.com
+ */
+
+/* HTML document is loaded. DOM is ready.
+-----------------------------------------*/
+// $(document).ready(function(){
+
+// 	// Mobile menu
+// 	$('.mobile-menu-icon').click(function(){
+// 		$('.tm-nav').slideToggle();
+// 	});
+
+// 	$( window ).resize(function() {
+// 		if($( window ).width() > 767) {
+// 			$('.tm-nav').show();
+// 		} else {
+// 			$('.tm-nav').hide();
+// 		}
+// 	});})
